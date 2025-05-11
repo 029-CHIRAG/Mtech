2abf3dd7ca5176ed478af057a5de51c2507481d7
Chirag Tomar <chiragtomar251@gmail.com>
2025-05-11 07:23:13 +0530

    Cors Updated

--- backend/index.js
+++ backend/index.js
@@ -24,13 +24,14 @@ const path = require('path');
 const app = express();
 
 app.use(express.json({ limit: "50mb" }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.options("*", cors());
 
 app.use((req, res, next) => {
