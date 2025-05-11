da651a9ad311cd819966b75bbb510eda5766d7f5
Chirag Tomar <chiragtomar251@gmail.com>
2025-05-11 07:43:19 +0530

    Newcourse

--- frontend/src/components/NewCourse.jsx
+++ frontend/src/components/NewCourse.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./NewCourse.css";
+import './NewCourse.css';
 
 const NewCourse = () => {
   const [courseData, setCourseData] = useState({
